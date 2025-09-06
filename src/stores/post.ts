2f7ba61c12099bad2c2bd5b13a051fe2a2f2ea06
yukion614 <34601876+yukion614@users.noreply.github.com>
2025-09-07 00:31:43 +0800

    增加header token

--- src/stores/post.ts
+++ src/stores/post.ts
@@ -26,13 +26,17 @@ export const usePostStore = defineStore("post", {
       const res = await fetch(`http://127.0.0.1:3000/api/posts/${id}`);
       this.post = await res.json();
     },
-    //---------------------------------
+    //發留言---------------------------------
     async submitComment(postId: number, content: string, authorId: number) {
       const api = `http://127.0.0.1:3000/api/comments/posts/${postId}/comments`;
+      const token = localStorage.getItem('token')
       try {
         const res = await fetch(api, {
           method: "POST",
-          headers: {"Content-Type": "application/json"},
+          headers: {
+            "Content-Type": "application/json",
+             "Authorization": `Bearer ${token}`
+          },
           body: JSON.stringify({ content,authorId }),
         });
       //刷新留言區畫面       
